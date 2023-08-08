130ebf056af8c9f4e2566a0e320eb27a15ba6d8f
Saharat Thongin <6306021620191@fitm.kmutnb.ac.th>
2023-08-08 16:13:07 +0700

    Fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: isProd ? 'https://github.com/dewviee/dewviee.github.io' : '',
+    assetPrefix: isProd ? '/dewviee.github.io/' : '',
 }
 
 module.exports = nextConfig
